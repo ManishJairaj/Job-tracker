a098cab8c1637e6a1051020635cbb31912d35571
Manish Jairaj <manishjr66@gmail.com>
2026-07-26 20:59:41 +0530

    Setup

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
 
 # models.Base.metadata.create_all(bind=engine)
-app = FastAPI()
+app = FastAPI(
+    title="AI Job Tracker API",
+    version="1.0.0"
+)
 
 @app.get("/")
 async def root():
     return {"message": "Hello World hiiii"}
+
+@app.get("/health")
+async def check_status():
+    return {"status" : "Healthy"}
